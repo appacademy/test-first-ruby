883fa5c5c09d82adbf262a03c013a8174fd65682
Jeffrey Liu <mangomystery@gmail.com>
2016-05-28 19:20:38 -0700

    05 solutions

--- lib/05_silly_blocks.rb
+++ lib/05_silly_blocks.rb
@@ -1,4 +1,17 @@
-def reverser(&prc)
-  prc.call.reverse
+def reverser
+  phrase = yield
+  arr = []
+  phrase.split.each do |word|
+    arr << word.reverse
+  end
+
+  arr.join(" ")
 end
 
+def adder(num = 1)
+  num + yield
+end
+
+def repeater(num = 1)
+  num.times { yield }
+end
