e404c5f7cd8553beb6d7fd65cfa059c0be83566c
KerryKDiehl <kerrykdiehl@gmail.com>
2016-07-21 17:43:14 -0700

    08 book titles

--- lib/08_book_titles.rb
+++ lib/08_book_titles.rb
@@ -1,3 +1,18 @@
 class Book
-  # TODO: your code goes here!
+  attr_reader :title
+  def title=(title)
+    final_title = ""
+    little_words = ["a","an","the","at","by","for","in","of","on","to","up","and","as","but","or","nor","over"]
+    title_array = title.split(" ")
+    title_array[0] = title_array[0].capitalize
+    title_array.each do |n|
+      if little_words.include?(n)
+        final_title = final_title + n + " "
+      else
+        final_title = final_title + n.capitalize + " "
+      end
+    end
+    @title = final_title.chomp(" ")
+  end
 end
+
