02b4988d22efc83f81c383e6170b93f8ba802f50
KerryKDiehl <kerrykdiehl@gmail.com>
2016-08-04 17:51:33 -0700

    12 RPN calculator

--- lib/12_rpn_calculator.rb
+++ lib/12_rpn_calculator.rb
@@ -1,3 +1,69 @@
+require 'byebug'
 class RPNCalculator
-  # TODO: your code goes here!
+  attr_accessor :input
+
+
+  def initialize
+    @input = []
+    @operators = [:+,:-,:*,:/]
+  end
+
+  def push(num)
+    @input.push(num.to_f)
+  end
+
+  def calculate(operator)
+    if @input.length > 1
+      @input.push(operator)
+    else
+      raise 'calculator is empty'
+    end
+  end
+
+  def plus
+    calculate(:+)
+  end
+
+  def minus
+    calculate(:-)
+  end
+
+  def times
+    calculate(:*)
+  end
+
+  def divide
+    calculate(:/)
+  end
+
+  def tokens(string)
+    string.split(' ').map do |n|
+      if @operators.include?(n.to_sym)
+        n.to_sym
+      else
+        n.to_f
+      end
+    end
+  end
+
+  def evaluate(string)
+    @input = tokens(string)
+    value
+  end
+
+  def value
+    location = 0
+    func_operators = @input & @operators
+    while func_operators.length > 0
+      if @input.include?(func_operators[0])
+        op = func_operators[0]
+        location = @input.index(op) - 2
+        nums = @input.slice!(location,2)
+        @input[location] = eval(nums[0].to_s + op.to_s + nums[1].to_s)
+      else
+        func_operators.shift
+      end
+    end
+    @input[location]
+  end
 end
