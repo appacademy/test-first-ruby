3ec7a57fadf02f408530b0a5c7ed42ae7c5c28b2
Anvar <anvararty@Anvars-MBP.localdomain>
2019-11-13 10:27:52 -0800

    Please enter the commit message for your changes. Lines starting
    Test commit_from_AnvarArt

---
+++ lib/01_temperature.rb
@@ -0,0 +1,7 @@
+def ftoc (fahrenheit)
+  celsius = ((fahrenheit - 32) * 5.0) / 9
+end
+
+def ctof (celsius)
+  fahrenheit = ((celsius * 9.0) / 5.0) + 32
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,15 @@
+def add (f,s)
+  add = f + s
+end
+
+def subtract (f,s)
+  sub = f - s
+end
+
+def sum (arr)
+
+  sum = 0
+
+arr.each {|a| sum+=a}
+sum
+end
